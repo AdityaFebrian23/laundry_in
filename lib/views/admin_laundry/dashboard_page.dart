d90c38370e23ddbf7ead40edac869f99b6697f59
haniffari3 <farhanif8090@gmail.com>
2025-10-30 18:41:06 +0700

    commit halaman laundry

--- lib/views/admin_laundry/dashboard_page.dart
+++ lib/views/admin_laundry/dashboard_page.dart
@@ -1,10 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'package:provider/provider.dart';
+import '../../providers/auth_provider.dart';
+import '../../utils/location_helper.dart';
 
-class AdminLaundryDashboard extends StatelessWidget {
-  const AdminLaundryDashboard({super.key});
+class PetugasDashboard extends StatefulWidget {
+  const PetugasDashboard({super.key});
+
+  @override
+  State<PetugasDashboard> createState() => _PetugasDashboardState();
+}
+
+class _PetugasDashboardState extends State<PetugasDashboard> {
+  IO.Socket? socket;
+  bool connected = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initSocket();
+  }
+
+  void _initSocket() {
+    final base = Uri.parse('http://10.0.2.2:4000');
+    socket = IO.io(base.toString(), <String, dynamic>{'transports': ['websocket']});
+    socket!.on('connect', (_) => setState(() => connected = true));
+    socket!.on('disconnect', (_) => setState(() => connected = false));
+  }
+
+  _sendLocation() async {
+    final pos = await LocationHelper.currentPosition();
+    final auth = Provider.of<AuthProvider>(context, listen: false);
+    if (pos != null && socket != null && socket!.connected) {
+      socket!.emit('location:update', {
+        'petugasId': auth.user?.id,
+        'lat': pos.latitude,
+        'lng': pos.longitude,
+        'orderId': null,
+        'userId': null
+      });
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Lokasi terkirim')));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text('Admin Laundry Dashboard')), body: const Center(child: Text('Fitur admin laundry: manage services, orders, staff')));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Petugas Dashboard'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () => Provider.of<AuthProvider>(context, listen: false).logout(context),
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Socket: ${connected ? "connected" : "disconnected"}'),
+            const SizedBox(height: 12),
+            ElevatedButton(onPressed: _sendLocation, child: const Text('Kirim Lokasi Sekarang'))
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    socket?.dispose();
+    super.dispose();
   }
 }
