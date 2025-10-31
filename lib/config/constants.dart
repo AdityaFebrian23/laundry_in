132d417c88dafea708b4d999fc510960f06f57c7
haniffari3 <farhanif8090@gmail.com>
2025-10-31 11:02:16 +0700

    semua

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,9 +1,5 @@
 class API {
-<<<<<<< HEAD
-  static const String HOST = '192.168.100.232'; // ⚠️ gunakan IP Wi-Fi kamu
-=======
-  static const String HOST = '192.168.100.161'; // IP dari log server Node.js
->>>>>>> 39e93bacd60cb914c974a99f068467b9b275d68e
+  static const String HOST = '192.168.100.232'; // IP laptop kamu
   static const String BASE_URL = 'http://$HOST:5000/api';
   static const String AUTH = '$BASE_URL/auth';
   static const String USERS = '$BASE_URL/users';
